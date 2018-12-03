9bba78c4fb1b0b6d454510ec436621d9f47870fc
smokewheateveryday <2135@estudantes.ips.pt>
2018-12-03 17:31:59 +0000

    Added sanity check to create_project_folders.sh

--- Database/create_project_folders.sh
+++ Database/create_project_folders.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
-mkdir -p ~/metagenomics_LAB/AG/fastq
-mkdir -p ~/metagenomics_LAB/AG/metadata
-mkdir -p ~/metagenomics_LAB/IBD/fastq
-mkdir -p ~/metagenomics_LAB/IBD/metadata
-mkdir -p ~/metagenomics_LAB/T2D/fastq
-mkdir -p ~/metagenomics_LAB/T2D/metadata
-mkdir -p ~/metagenomics_LAB/DB
+if [ ! -d "~/metagenomics_LAB/" ]
+	then
+		mkdir -p ~/metagenomics_LAB/AG/fastq
+		mkdir -p ~/metagenomics_LAB/AG/metadata
+		mkdir -p ~/metagenomics_LAB/IBD/fastq
+		mkdir -p ~/metagenomics_LAB/IBD/metadata
+		mkdir -p ~/metagenomics_LAB/T2D/fastq
+		mkdir -p ~/metagenomics_LAB/T2D/metadata
+		mkdir -p ~/metagenomics_LAB/DB
+	else
+		echo "You already have the project folders, dumb-dumb!"
