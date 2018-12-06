1aac64ad3b6e6dcf126e5e2ecd51c416203ee20a
smokewheateveryday <2135@estudantes.ips.pt>
2018-12-06 13:54:00 +0000

    Added Y/N prompts to the database install script

--- workflow-database-install.sh
+++ workflow-database-install.sh
@@ -6,27 +6,99 @@ echo "Let's hope this works boys"
     chmod +x Database/Import_data_AG.py
     chmod +x Database/Import_data_IBD.py
     chmod +x Database/Import_data_T2D.py
-    chmod +x Database/Import_AG_metadata.py
-    chmod +x Database/Import_IBD_metadata.py
-    chmod +x Database/Import_T2D_metadata.py
-    chmod +x Database/xml_AG_tocollection.py
+    chmod +x Database/Import_AG_metadata_db.py
+    chmod +x Database/Import_IBD_metadata_db.py
+    chmod +x Database/Import_T2D_metadata_db.py
+    chmod +x Database/xml_AG_tocollection_db.py
+
     echo "Creating project folders..."
     ./Database/create_project_folders.sh
     echo "Creating MongoDB collections..."
     ./Database/create_mongo_db_collections.sh
-    echo "Importing AG metadata..."
-    python Database/Import_data_AG.py
-    echo "Importing IBD data..."
-    python Database/Import_data_IBD.py
-    echo "Importing T2D data..."
-    python Database/Import_data_T2D.py
-    echo "Adding AG metadata to collection..."
-    python Database/Import_AG_metadata.py
-    echo "Adding IBD metadata to collection..."
-    python Database/Import_IBD_metadata.py
-    echo "ADding T2D metadata to collection..."
-    python Database/Import_T2D_metadata.py
-    echo "Installing Kraken and the Greengenes database (here's where it starts taking a while!)"
-    ./Database/create_kraken_greengenes.sh
+
+while true
+do
+  read -r -p "Would you like to download the AG database? [y/n]" input1
+
+  case $input1 in
+		[yY][eE][sS]|[yY])
+    	echo "Importing AG data..."
+    	python Database/Import_data_AG.py
+    	echo "Adding AG metadata to collection..."
+    	python Database/Import_AG_metadata_db.py
+	break
+	;;
+		[nN][oO]|[nN])
+	echo "Skipping AG database..."
+	break
+	;;
+		*)
+	echo "Invalid input."
+	;;
+	esac
+done
+while true
+do
+  read -r -p "Would you like to download the IBD database? [y/n]" input1
+
+  case $input1 in
+		[yY][eE][sS]|[yY])
+    	echo "Importing AG data..."
+    	python Database/Import_data_IBD.py
+    	echo "Adding AG metadata to collection..."
+    	python Database/Import_IBD_metadata_db.py
+	break
+	;;
+		[nN][oO]|[nN])
+	echo "Skipping IBD database..."
+	break
+	;;
+		*)
+	echo "Invalid input."
+	;;
+	esac
+done
+
+while true
+do
+  read -r -p "Would you like to download the T2D database? [y/n]" input1
+
+  case $input1 in
+		[yY][eE][sS]|[yY])
+    	echo "Importing T2D data..."
+    	python Database/Import_data_T2D.py
+    	echo "Adding AG metadata to collection..."
+    	python Database/Import_T2D_metadata_db.py
+	break
+	;;
+		[nN][oO]|[nN])
+	echo "Skipping T2D database..."
+	break
+	;;
+		*)
+	echo "Invalid input."
+	;;
+	esac
+done
+
+while true
+do
+  read -r -p "Would you like to install the Kraken and the Greengenes database? [y/n]" input1
+
+  case $input1 in
+		[yY][eE][sS]|[yY])
+    	echo "Installing Kraken and the Greengenes database..."
+    	./Database/create_kraken_greengenes.sh
+	break
+	;;
+		[nN][oO]|[nN])
+	echo "Skipping Kraken database..."
+	break
+	;;
+		*)
+	echo "Invalid input."
+	;;
+	esac
+done
     echo "Script finished running in $SECONDS seconds."
 break
