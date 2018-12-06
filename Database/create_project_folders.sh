ffd89a75febaab2cc71736bd89acfb7bd75120b6
smokewheateveryday <2135@estudantes.ips.pt>
2018-12-06 14:02:18 +0000

    Script now checks if the folders have been created

--- Database/create_project_folders.sh
+++ Database/create_project_folders.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 
-mkdir -p ~/metagenomics_LAB/AG/fastq
-mkdir -p ~/metagenomics_LAB/AG/metadata
-mkdir -p ~/metagenomics_LAB/IBD/fastq
-mkdir -p ~/metagenomics_LAB/IBD/metadata
-mkdir -p ~/metagenomics_LAB/T2D/fastq
-mkdir -p ~/metagenomics_LAB/T2D/metadata
-mkdir -p ~/metagenomics_LAB/DB
+PROJECT_FOLDER=~/metagenomics_LAB
+
+if [ ! -d "$PROJECT_FOLDER" ]
+	then
+		echo "Creating project folders..."
+			mkdir -p ~/metagenomics_LAB/AG/fastq
+			mkdir -p ~/metagenomics_LAB/AG/metadata
+			mkdir -p ~/metagenomics_LAB/IBD/fastq
+			mkdir -p ~/metagenomics_LAB/IBD/metadata
+			mkdir -p ~/metagenomics_LAB/T2D/fastq
+			mkdir -p ~/metagenomics_LAB/T2D/metadata
+			mkdir -p ~/metagenomics_LAB/DB
+	else
+		echo "The folders have already been created!"
+fi
 
 
