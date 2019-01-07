dc30692c50576e0244fd41344d7f80d708ddf18f
Rato2392 <45361625+Rato2392@users.noreply.github.com>
2019-01-07 11:12:51 +0000

    Update ete3tree.py
    add try except command to make tree without the taxonomic ids that are not in ncbi taxonomic database

--- Taxonomy/ete3tree.py
+++ Taxonomy/ete3tree.py
@@ -1,6 +1,7 @@
 from ete3 import NCBITaxa
 ncbi = NCBITaxa()
 ncbi.update_taxonomy_database()
+built = False
 
 #format=1 aparecer nomes dos nodes interiores
 infile2 = open("colunasfinal", 'r')
@@ -10,24 +11,18 @@ for lines in infile2:
 
 infile2.close()
 
-t = ncbi.get_topology(idtaxa, intermediate_nodes=True)
+while not built:
+	try:
+		t = ncbi.get_topology(idtaxa, intermediate_nodes=True)
 
-treefinal = t.get_ascii(attributes=["sci_name"])
-
+		treefinal = t.get_ascii(attributes=["rank", "sci_name"])
+		built= True
+	except KeyError as e:
+		taxid_not_found = int(e.args[0])
+		idtaxa.remove(taxid_not_found)
+		print("the following IDs were not found in NCBI taxonomy database:" + str(taxid_not_found))
+		pass
 
 		
 with open("ete3tree", 'w') as tree:
 	print(treefinal, file = tree)
-	
-
-
-
-
-
-
-
-
-
-	
-
-
