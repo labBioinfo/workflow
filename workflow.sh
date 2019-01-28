32067e93ac2d27caf4eeeb56fd67c3dffa5cc825
Bobo <40632365+smokewheateveryday@users.noreply.github.com>
2019-01-28 10:23:42 +0000

    workflow.sh updated to integrate all scripts (at last!)

--- workflow.sh
+++ workflow.sh
@@ -4,7 +4,7 @@
 
 #Workflow menu
 PS3='Welcome to the BioInfo lab project workflow! Please select an option: '
-options=("Install dependencies and download database files" "Check and install Python dependencies" "Run basic workflow test" "Quit")
+options=("Install dependencies and download database files" "Install conda" "Check and install Python dependencies" "Taxonomy report" "Diversity analysis" "Logging report" "Quit")
 select opt in "${options[@]}"
 do
     case $opt in
@@ -12,7 +12,12 @@ do
 	    chmod +x workflow-database-install.sh
 	    ./workflow-database-install.sh
             ;;
-        "Check and install Python dependencies")
+    	"Install conda")
+	     wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+	     chmod +x Miniconda3-latest-Linux-x86_64.sh
+	     ./Miniconda3-latest-Linux-x86_64.sh
+	    ;;
+    	"Check and install Python dependencies")
 	    chmod +x workflow-create-conda-environments.sh
 	    chmod +x workflow-install-python-dependencies.py
 	    chmod +x workflow-conda-env-ete3.sh
@@ -26,12 +31,15 @@ do
             chmod +x workflow-taxonomy.sh #Kraken_output, colunas, ete3bubble, ete3tree, in that order
 	    ./workflow-taxonomy.sh
             ;;
-        "Quit")
+   	 "Diversity analysis")
+	    Rscript ./DiversityAnalysis/script_for_stat_analysis.r
+	    ;;
+    	"Logging report")
+	    Rscript ./Logging/report.R
+	    ;;
+    	"Quit")
             break
             ;;
         *) echo "invalid option $REPLY";;
     esac
 done
-#NEXT: Finish up the database install, test it once or twice, PLAY ARCANUM#
-
-
